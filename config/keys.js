9ecd4ae09933fb322b47b2a2264157492edc401d
yale918 <yale918@gmail.com>
2021-06-19 00:53:03 +0800

    not mailer yet, search user ok

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod')
+}else{
+    module.exports = require('./dev')
+}
+
+// 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
